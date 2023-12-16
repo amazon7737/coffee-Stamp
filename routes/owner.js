d539f6519093fc3506b53425530e4ec02319c05c
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2023-12-17 03:00:41 +0900

    new

--- routes/owner.js
+++ routes/owner.js
@@ -15,22 +15,26 @@ const wdate = year + "-" + month + "-" + date;
 /* GET home page. */
 router.get("/mypage/1", async (req, res) => {
   const user_id = req.session.user_id;
+
+  // 유저 id 찾기
   const owner_id = await pool.query(
     "select id user_id from user where login_id = ?",
     [user_id]
   );
 
+  // 카페 - 점주 체크
   const cafeCheck = await pool.query(
     "select * from dbseven.cafe where owner_id =?",
     [owner_id[0][0].user_id]
   );
 
+  //
   const cafe = await pool.query(
     "SELECT a.*, b.*, c.* FROM dbseven.owner a inner join dbseven.user b on a.user_id = b.id inner join dbseven.cafe c on a.cafe_id = c.id where user_id = ?;",
     [owner_id[0][0].user_id]
   );
 
-  console.log(cafe[0]);
+  //   console.log(cafe[0]);
 
   const sales = await pool.query(
     "select * from dbseven.monthly_record where cafe_id = ?;",
@@ -60,7 +64,32 @@ router.get("/mypage/4", async (req, res) => {
     "select * from dbseven.cafe_propensity where cafe_id =?",
     [Number(owner[0][0].cafe_id)]
   );
+
   console.log("propen:", propen[0]);
+  //   console.log("owner::", owner[0][0].cafe_id);
+
+  // 매출
+  const sales = await pool.query(
+    "select * from dbseven.monthly_record where cafe_id = ?;",
+    [Number(owner[0][0].cafe_id)]
+  );
+  //   console.log(sales[0]);
+
+  //성향 가입 날짜
+  const register_time = propen[0][0].create_time;
+  // 성향 가입 전 매출
+  const no_register = [];
+  const after_register = [];
+
+  for (var i = 0; i < sales[0].length; i++) {
+    console.log(Date.parse(register_time));
+    console.log("sales:", i, sales[0][i].create_time);
+    // if()
+  }
+
+  // 성향 가입 후 매출
+
+  //   console.log("sales:", sales[0]);
 
   // 성향 json
   // 인테리어
@@ -94,26 +123,29 @@ router.get("/mypage/4", async (req, res) => {
 
   // 오늘 날짜 - 만료 월
   const expire = Number(month) - Number(expire_month);
+
   //패키지 사이즈
   const package_time = Number(propen[0][0].package_size);
 
   // 만료일 연산
   const result_time = package_time - expire;
 
-  console.log("result_time:", result_time);
-  const result = [
-    {
-      create_time: propen[0][0].create_time,
-      result_time: result_time,
-    },
-  ];
-
   // 성향 가입을 안했다면
   if (Number(partnerShip[0][0].partnership) == 0) {
     return res.render("cafe_mypage_attract_sign", {});
   } else {
     // 성향 가입을 했다면
+
     // 날짜가 가입일 보내주고 / 성향 서비스 만료일
+
+    // 결과 json
+    const result = [
+      {
+        create_time: propen[0][0].create_time,
+        result_time: result_time,
+      },
+    ];
+
     return res.render("cafe_mypage_attract", { sales: "", result: result[0] });
 
     // res.render("cafe_mypage_atrract", {});
